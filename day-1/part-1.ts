a754a72b17d3ff322cd1a90cae6210a3b61bcb4a
Ben Hancock <id@benhancock.com>
2022-12-02 10:48:21 +1100

    complete day 1

---
+++ day-1/part-1.ts
@@ -0,0 +1,29 @@
+export const a = 1;
+
+const input = await Deno.readTextFile("./input.txt");
+
+const foodByElf = input
+  .split("\n\n")
+  .map((allFoodStr) => allFoodStr.split("\n").map((foodStr) => +foodStr));
+
+const totalCaloriesByElf = foodByElf.map((allFood) => {
+  let totalCalories = 0;
+  allFood.forEach((food) => (totalCalories += food));
+  return totalCalories;
+});
+
+let highestCalorieElfIndex = 0;
+let highestCalorieElfCalories = 0;
+
+totalCaloriesByElf.forEach((calories, i) => {
+  if (calories > highestCalorieElfCalories) {
+    highestCalorieElfCalories = calories;
+    highestCalorieElfIndex = i;
+  }
+});
+
+console.log(`The highest calorie elf is ${highestCalorieElfIndex + 1}`);
+
+console.log(
+  `The highest calorie elf has ${highestCalorieElfCalories} calories`
+);
---
+++ day-1/part-2.ts
@@ -0,0 +1,25 @@
+export const a = 1;
+
+const input = await Deno.readTextFile("./input.txt");
+
+const foodByElf = input
+  .split("\n\n")
+  .map((allFoodStr) => allFoodStr.split("\n").map((foodStr) => +foodStr));
+
+const totalCaloriesByElf = foodByElf.map((allFood) => {
+  let totalCalories = 0;
+  allFood.forEach((food) => (totalCalories += food));
+  return totalCalories;
+});
+
+const totalCaloriesByElfOrdered = totalCaloriesByElf
+  .sort((a, b) => a - b)
+  .reverse();
+
+const topThreeElvesTotal = totalCaloriesByElfOrdered
+  .slice(0, 3)
+  .reduce((a, b) => a + b, 0);
+
+console.log(
+  `The top 3 elves are carrying a total of ${topThreeElvesTotal} calories`
+);
